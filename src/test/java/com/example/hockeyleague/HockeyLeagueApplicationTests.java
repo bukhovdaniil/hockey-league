adf9748acb9f2761b765792f25d5ee994aa20fd3
Daniil <danyadb@yandex.ru>
2020-05-05 17:07:28 +0300

    Add general things

---
+++ src/test/java/com/example/hockeyleague/HockeyLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hockeyleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
